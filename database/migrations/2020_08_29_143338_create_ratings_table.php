056a1cdc533e49d9557258810ba155cca786706c
Fadil S Hardy <xaber255@gmail.com>
2020-08-29 21:57:33 +0700

    Add Models & Migrations

---
+++ database/migrations/2020_08_29_143338_create_ratings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->integer('rate');
+            $table->longText('comment')->Nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('movie_id')
+                ->references('id')
+                ->on('movies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
